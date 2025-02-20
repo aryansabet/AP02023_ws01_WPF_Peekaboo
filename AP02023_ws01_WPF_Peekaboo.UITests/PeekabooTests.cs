d49edd4d0fedcab53100399fad668fd9d9584d69
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2025-02-20 15:26:09 +0330

    feat: add new tests

--- AP02023_ws01_WPF_Peekaboo.UITests/PeekabooTests.cs
+++ AP02023_ws01_WPF_Peekaboo.UITests/PeekabooTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
     using System.Reflection;
     using System.Windows.Automation.Peers;
     using System.Windows.Automation.Provider;
+    using System.Xml.Linq;
     using Xunit;
 
 
@@ -51,28 +53,15 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
 
         }
 
-        //[WpfTheory]
-        //[InlineData("LeftBtn")]
-        //[InlineData("RightBtn")]
-        //public void ShouldAttach_ClickHandlersToButtons(string ButtonName)
-        //{
-        //    var window = new MainWindow();
-
-        //    var Btn = window.FindName(ButtonName) as Button;
-        //    Assert.NotNull(Btn.Click);
-
-        //}
-
-
-
-
 
         [WpfTheory]
-        [InlineData(["Images/Red.png", "LeftBtn"])]
-        [InlineData(["Images/Yellow.png", "RightBtn"])]
+        [InlineData("Images/Red.png", "LeftBtn")]
+        [InlineData("Images/Yellow.png", "RightBtn")]
         public void ShouldHave_CorrectImageOnCorrectButton(string ImageAddress, string ButtonName)
         {
             var mainWindow = new MainWindow();
+            mainWindow.Show();
+
             var Btn = mainWindow.FindName(ButtonName) as Button;
 
             Assert.IsType<Image>(Btn.Content);
@@ -85,7 +74,38 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
 
             Assert.Contains(ImageAddress, Image.Source.ToString());
         }
+        [Fact]
+        public void Xaml_ShouldHaveExpectedEvents()
+        {
+            var xamlPath = FindMainWindowXamlPath();
+            var doc = XDocument.Load(xamlPath);
 
+            // 1. Check Button x:Name="LeftBtn" has Click="LeftButtonClickHandle"
+            var leftButton = doc.Descendants()
+                .FirstOrDefault(e => e.Name.LocalName == "Button" &&
+                    e.Attributes().Any(a => a.Name.LocalName == "Name" && a.Value == "LeftBtn"));
+            Assert.NotNull(leftButton);
+            Assert.Equal("LeftButtonClickHandle", leftButton.Attribute("Click")?.Value);
+
+            // 2. Check Button x:Name="RightBtn" has Click="RightButtonClickHandle"
+            var rightButton = doc.Descendants()
+                .FirstOrDefault(e => e.Name.LocalName == "Button" &&
+                    e.Attributes().Any(a => a.Name.LocalName == "Name" && a.Value == "RightBtn"));
+            Assert.NotNull(rightButton);
+            Assert.Equal("RightButtonClickHandle", rightButton.Attribute("Click")?.Value);
+
+            // 3. Check <Image Loaded="LeftImageLoaded" ...>
+            //var leftImage = doc.Descendants()
+            //    .FirstOrDefault(e => e.Name.LocalName == "Image" &&
+            //        e.Attribute("Loaded")?.Value == "LeftImageLoaded");
+            //Assert.NotNull(leftImage);
+
+            //// 4. Check <Image Loaded="RightImageLoaded" ...>
+            //var rightImage = doc.Descendants()
+            //    .FirstOrDefault(e => e.Name.LocalName == "Image" &&
+            //        e.Attribute("Loaded")?.Value == "RightImageLoaded");
+            //Assert.NotNull(rightImage);
+        }
 
         [WpfFact]
         public void ShouldAttachClickHandlerToRightBtn()
@@ -104,13 +124,37 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
 
         }
 
+        [WpfFact]
+        public void ShouldNotHaveStaticMemberVariable()
+        {
+            // ASK WHY?
+            Type myType = typeof(MainWindow);
+
+            // Get all static members of MyClass
+            FieldInfo[] staticFields = myType.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            // Display the names of all static members
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            foreach (FieldInfo field in staticFields)
+            {
+                object value = field.GetValue(null);
+                map.Add(field.Name, value.ToString());
+            }
+            // "Must have no static variables"
+            Assert.Empty(map);
+
+            //foreach (string key in map.Keys)
+            //{
+            //    Console.WriteLine("{0}={1}", key, map[key].ToString());
+            //}
+
+        }
 
 
         [WpfFact]
         public void ButtonsShouldSwapImagesAfter1Click()
         {
             var mainWindow = new MainWindow();
-            //mainWindow.Show();
+            mainWindow.Show();
 
             var Btn = mainWindow.FindName("RightBtn") as Button;
             Assert.NotNull(Btn);
@@ -139,7 +183,7 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
         public void ButtonsShouldSwapImagesAfter2Clicks()
         {
             var mainWindow = new MainWindow();
-            //mainWindow.Show();
+            mainWindow.Show();
 
             var Btn = mainWindow.FindName("RightBtn") as Button;
             Assert.NotNull(Btn);
@@ -172,7 +216,7 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
         public void ShouldScoreUpAfter1Click()
         {
             var mainWindow = new MainWindow();
-            //mainWindow.Show();
+            mainWindow.Show();
 
             var Btn = mainWindow.FindName("RightBtn") as Button;
             Assert.NotNull(Btn);
@@ -194,7 +238,7 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
         public void ShouldScoreUpAfter2Clicks()
         {
             var mainWindow = new MainWindow();
-            //mainWindow.Show();
+            mainWindow.Show();
 
             var Btn = mainWindow.FindName("RightBtn") as Button;
             Assert.NotNull(Btn);
@@ -222,7 +266,7 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
         public void ShouldNotActAfterClickRedButton()
         {
             var mainWindow = new MainWindow();
-            //mainWindow.Show();
+            mainWindow.Show();
 
             var Btn = mainWindow.FindName("RightBtn") as Button;
             Assert.NotNull(Btn);
@@ -265,6 +309,35 @@ namespace AP02023_ws01_WPF_Peekaboo.UITests
             Assert.Equal("2", Textblock.Text);
         }
 
+
+        public static string FindMainWindowXamlPath()
+        {
+            // Optionally, you could accept a parameter for the starting directory
+            var startDirectory = Directory.GetCurrentDirectory();
+            var dirInfo = new DirectoryInfo(startDirectory);
+
+            while (dirInfo != null)
+            {
+                // 1) Look for a subfolder named "AP02023_ws01_WPF_Peekaboo" in the current directory
+                string targetFolder = Path.Combine(dirInfo.FullName, "AP02023_ws01_WPF_Peekaboo");
+                if (Directory.Exists(targetFolder))
+                {
+                    // 2) Check if "MainWindow.xaml" exists in that subfolder
+                    string xamlPath = Path.Combine(targetFolder, "MainWindow.xaml");
+                    if (File.Exists(xamlPath))
+                    {
+                        return xamlPath;
+                    }
+                }
+
+                // Move up one level
+                dirInfo = dirInfo.Parent;
+            }
+
+            // If we exit the loop, we never found the file
+            throw new FileNotFoundException(
+                $"Could not find 'MainWindow.xaml' in a folder named 'AP02023_ws01_WPF_Peekaboo' while searching upward from '{startDirectory}'.");
+        }
     }
 
 }
